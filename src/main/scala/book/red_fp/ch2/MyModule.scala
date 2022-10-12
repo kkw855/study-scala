dc6584e8e8ae9a4eba42f25ce29f0c134ae95c26
kkw855 <kkw855@gmail.com>
2022-10-12 17:37:38 +0900

    Working...

--- src/main/scala/book/red_fp/ch2/MyModule.scala
+++ src/main/scala/book/red_fp/ch2/MyModule.scala
@@ -2,6 +2,7 @@ package book.red_fp.ch2
 
 import scala.annotation.tailrec
 
+//noinspection DuplicatedCode
 private object MyModule:
   private def abs(a: Int) =
     if a < 0 then -a
@@ -18,9 +19,42 @@ private object MyModule:
       else go(n - 1, n * acc)
     go(n, 1)
 
+  def formatFactorial(n: Int): String =
+    val msg = "The factorial of %d is %d."
+    msg.format(n, factorial(n))
+
+  // Exercise 2.1
   def fibonacci(n: Int): Int =
     @tailrec
     def go(n: Int, prev: Int, cur: Int): Int =
       if n <= 0 then prev
       else go(n - 1, cur, prev + cur)
     go(n, 0, 1)
+
+  def formatResult(n: Int, f: Int => String): String =
+    f(n)
+
+  def findFirstMono(ss: Array[String], key: String): Int =
+    @tailrec
+    def loop(n: Int): Int =
+      if n >= ss.length then -1
+      else if ss(n) == key then n
+      else loop(n + 1)
+    loop(0)
+
+  def findFirstPoly[A](ss: Array[A])(p: A => Boolean): Int =
+    @tailrec
+    def loop(n: Int): Int =
+      if n >= ss.length then -1
+      else if p(ss(n)) then n
+      else loop(n + 1)
+    loop(0)
+
+  // Exercise 2.2
+  def isSorted[A](ss: Array[A])(p: (A, A) => Boolean): Boolean =
+    @tailrec
+    def loop(n: Int): Boolean =
+      if n >= (ss.length - 1) then true
+      else if !p(ss(n), ss(n + 1)) then false
+      else loop(n + 1)
+    loop(0)
--- src/test/scala/book/red_fp/ch2/MyModuleTest.scala
+++ src/test/scala/book/red_fp/ch2/MyModuleTest.scala
@@ -21,6 +21,11 @@ class MyModuleTest extends AnyFunSuite, Matchers:
     factorial(4) shouldBe 24
   }
 
+  test("formatFactorial") {
+    formatFactorial(4) shouldBe "The factorial of 4 is 24."
+  }
+
+  // Exercise 2.1
   test("fibonacci") {
     fibonacci(-1) shouldBe 0
     fibonacci(0) shouldBe 0
@@ -30,3 +35,29 @@ class MyModuleTest extends AnyFunSuite, Matchers:
     fibonacci(4) shouldBe 3
     fibonacci(5) shouldBe 5
   }
+
+  test("formatResult") {
+    formatResult(-42, formatAbs) shouldBe "The absolute value of -42 is 42."
+    formatResult(4, formatFactorial) shouldBe "The factorial of 4 is 24."
+  }
+
+  test("findFirstMono") {
+    findFirstMono(Array("hi", "hello", "world"), "unknown") shouldBe -1
+    findFirstMono(Array("hi", "hello", "world"), "hello") shouldBe 1
+  }
+
+  test("findFirstPoly") {
+    findFirstPoly(Array("hi", "hello", "world"))(
+      _.contains("unknown")
+    ) shouldBe -1
+    findFirstPoly(Array("hi", "hello", "world"))(_.contains("llo")) shouldBe 1
+
+    findFirstPoly(Array(1, 2, 3))(_ > 100) shouldBe -1
+    findFirstPoly(Array(1, 2, 3))(_ > 1) shouldBe 1
+  }
+
+  // Exercise 2.2
+//  test("isSorted") {
+//    isSorted(Array(1, 2, 4, 3)) shouldBe false
+//    isSorted(Array(1, 2, 4, 3)) shouldBe false
+//  }
