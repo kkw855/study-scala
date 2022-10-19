6ab48e69c3fe24631dab329fe8defc8140d8ee32
kkw855 <kkw855@gmail.com>
2022-10-19 13:32:51 +0900

    Working...

--- src/main/scala/book/red_fp/ch4/Option.scala
+++ src/main/scala/book/red_fp/ch4/Option.scala
@@ -30,9 +30,35 @@ object Option:
     _ map f
 
   def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
-    (a, b) match
-      case (Some(a), Some(b)) => Some(f(a, b))
-      case _                  => None
+    a.flatMap(aa => b.map(bb => f(aa, bb)))
 
   def insuranceRateQuote(age: Int, numberOfSpeedingTickets: Int): Double =
-    age + numberOfSpeedingTickets * 1.3
+    (age + numberOfSpeedingTickets) * 1.3
+
+  def parseInsuranceRateQuote(
+      age: String,
+      numberOfSpeedingTickets: String
+  ): Option[Double] =
+    val optAge = Try { age.toInt }
+    val optTickets = Try { numberOfSpeedingTickets.toInt }
+    map2(optAge, optTickets)(insuranceRateQuote)
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match
+    case Nil    => Some(Nil)
+    case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
+
+  def sequence_1[A, B](a: List[Option[A]]): Option[List[A]] =
+    a.foldRight[Option[List[A]]](Some(Nil))((x, y) => map2(x, y)(_ :: _))
+
+  def parseInts(a: List[String]): Option[List[Int]] =
+    sequence(a map { aa => Try(aa.toInt) })
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match
+    case Nil    => Some(Nil)
+    case h :: t => map2(f(h), traverse(t)(f))(_ :: _)
+
+  def traverse_1[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    a.foldRight[Option[List[B]]](Some(Nil))((x, y) => map2(f(x), y)(_ :: _))
+
+  def sequenceViaTraverse[A, B](a: List[Option[A]]): Option[List[A]] =
+    traverse(a)(x => x)
--- src/test/scala/book/red_fp/ch4/OptionTest.scala
+++ src/test/scala/book/red_fp/ch4/OptionTest.scala
@@ -4,3 +4,9 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class OptionTest extends AnyFunSuite, Matchers:
+  import Option._
+
+  test("parseInsuranceRateQuote") {
+    parseInsuranceRateQuote("2", "hello") shouldBe None
+    parseInsuranceRateQuote("2", "3") shouldBe Some(6.5)
+  }
