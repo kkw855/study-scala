8a62111b47f41a22972c0b7f1878319ee7d25214
kkw855 <kkw855@gmail.com>
2022-10-21 17:20:32 +0900

    Working...

--- src/main/scala/book/red_fp/ch4/Either.scala
+++ src/main/scala/book/red_fp/ch4/Either.scala
@@ -8,20 +8,20 @@ sealed trait Either[+E, +A]:
   def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match
     case Left(e) => Left(e)
     case Right(a) => f(a)
-  
+
   def orElse[EE >: E, AA >: A](b: => Either[EE, AA]): Either[EE, AA] = this match
     case Left(_) => b
     case Right(a) => Right(a)
- 
+
   def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
     this flatMap (aa => b map (f(aa, _)))
-    
+
   def map2_2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
-    for 
+    for
       aa <- this
       bb <- b
-    yield f(aa, bb)  
-  
+    yield f(aa, bb)
+
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
 
@@ -31,10 +31,20 @@ object Either:
     catch { case e: Exception => Left(e) }
 
   def insuranceRateQuote(age: Int, numberOfSpeedingTickets: Int): Double =
-    (age + numberOfSpeedingTickets) * 1.3  
-    
+    (age + numberOfSpeedingTickets) * 1.3
+
   def parseInsuranceRateQuote(age: String, numberOfSpeedingTickets: String): Either[Exception, Double] =
     for
       a <- Try { age.toInt }
       tickets <- Try { numberOfSpeedingTickets.toInt }
-    yield insuranceRateQuote(a, tickets)  
+    yield insuranceRateQuote(a, tickets)
+
+  def traverse[E, A, B](l: List[A])(f: A => Either[E, B]): Either[E, List[B]] = l match
+    case Nil => Right(Nil)
+    case h :: t => f(h) flatMap (hh => traverse(t)(f) map (hh :: _))
+
+  def sequence[E, A](l: List[Either[E, A]]): Either[E, List[A]] = l match
+    case Nil => Right(Nil)
+    case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
+
+
